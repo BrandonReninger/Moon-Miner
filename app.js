21c8c1561dcd62f6b69bdbe2c267d685bf8b40b5
Brandon Reninger <brandonreninger@yahoo.com>
2020-03-23 22:55:02 -0600

    fixed mine function, kinda hacky though

--- app.js
+++ app.js
@@ -41,36 +41,12 @@ let automaticUpgrades = {
 };
 
 
+
 function mine() {
-    let total = 1
-    let quantity = 0
-
-    for (const key in clickUpgrades) {
-        if (clickUpgrades["applied"] === true) {
-            quantity += quantity;
-            total = clickUpgrades["multiplier"] * quantity
-        }
-    }
-    /*if (inventory.pickaxe >= 1 && inventory.rover >= 1 && inventory.moonBase >= 1) {
-        inventory.gold += 62
-    } else if (inventory.pickaxe >= 1 && inventory.rover >= 1 && inventory.moonBase === 0) {
-        inventory.gold += 12
-    } else if (inventory.pickaxe >= 1 && inventory.moonBase >= 1 && inventory.rover === 0) {
-        inventory.gold += 52
-    } else if (inventory.rover >= 1 && inventory.moonBase >= 1 && inventory.pickaxe === 0) {
-        inventory.gold += 60
-    } else if (inventory.rover >= 1 && inventory.moonBase === 0 && inventory.pickaxe === 0) {
-        inventory.gold += 10
-    } else if (inventory.moonBase >= 1 && inventory.rover === 0 && inventory.pickaxe === 0) {
-        inventory.gold += 50
-    } else if (inventory.pickaxe >= 1 && inventory.rover === 0 && inventory.moonBase === 0) {
-        inventory.gold += 2
-    }*/
-
-
-    // FIXME iterate through clickupgrades and build a sum
-
-    inventory.gold += total
+    inventory.gold += 1;
+    inventory.gold += (clickUpgrades.pickaxe.quantity * clickUpgrades.pickaxe.multiplier);
+    inventory.gold += (clickUpgrades.rover.quantity * clickUpgrades.rover.multiplier);
+    inventory.gold += (clickUpgrades.moonBase.quantity * clickUpgrades.moonBase.multiplier);
 
 
     update()
@@ -93,7 +69,7 @@ function addPickaxe() {
 
 
 function addRover() {
-    if (inventory.gold >= clickUpgrades.addRover.price) {
+    if (inventory.gold >= clickUpgrades.rover.price) {
         inventory.rover += 1
         inventory.gold -= 20
         clickUpgrades.rover.quantity += 1
@@ -123,9 +99,47 @@ function addAliens() {
         automaticUpgrades.aliens.price *= 2
         // FIXME increment quantity 
     }
-    startInterval()
+
     update()
 }
+
+
+let collectionInterval = 0
+
+function startInterval() {
+    collectionInterval = setInterval(collectAutoUpgrades, 3000)
+}
+// START Interval 
+
+
+
+function collectAutoUpgrades() {
+    for (const quantity in automaticUpgrades) {
+        if (automaticUpgrades.hasOwnProperty(quantity)) {
+            const element = automaticUpgrades[key];
+            console.log(automaticUpgrades.quantity)
+
+        }
+    }
+}
+
+
+
+function update() {
+    let template = ""
+
+    template += /*html*/ `
+    <div class="col-4" id="update">
+    <h3 class="font-weight-bold"><span>Inventory</span></h3>
+    <h5>Gold: ${inventory.gold} </h5>
+    <h5>Pick Axe: ${inventory.pickaxe} </h5>
+    <h5>Rover: ${inventory.rover}</h5>
+    <h5>Moon Bases: ${inventory.moonBase}</h5>
+    </div>
+    `
+    let mytemplate = document.getElementById("update").innerHTML = template
+}
+
 
 //TODO get this function to work when applied
 /*function decrementClickUpgrades() {
@@ -141,39 +155,3 @@ function addAliens() {
         inventory.gold += 1
     }
 } */
-
-let collectionInterval = 0
-
-function startInterval() {
-    collectionInterval = setInterval(collectAutoUpgrades, 3000)
-}
-// START Interval 
-
-
-function collectAutoUpgrades() {
-    for (const key in automaticUpgrades) {
-        if (automaticUpgrades.hasOwnProperty(key)) {
-            const element = automaticUpgrades[key];
-            console.log(automaticUpgrades.quantity)
-            //FIXME collect based on the quantity 
-
-        }
-    }
-}
-
-
-
-function update() {
-    let template = ""
-
-    template += /*html*/ `
-   <div class="col-4" id="update">
-                <h3 class="font-weight-bold"><span>Inventory</span></h3>
-                <h5>Gold: ${inventory.gold} </h5>
-                <h5>Pick Axe: ${inventory.pickaxe} </h5>
-                <h5>Rover: ${inventory.rover}</h5>
-                <h5>Moon Bases: ${inventory.moonBase}</h5>
-            </div>
-    `
-    let mytemplate = document.getElementById("update").innerHTML = template
-}
